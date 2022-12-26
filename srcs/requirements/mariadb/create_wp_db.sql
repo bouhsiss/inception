16853d8f25893ba804791d225d30405ac6aa3ebe
Houda Bouhsiss <hbouhsis@e2r2p8.1337.ma>
2022-12-26 15:17:12 +0100

    testing

--- srcs/requirements/mariadb/create_wp_db.sql
+++ srcs/requirements/mariadb/create_wp_db.sql
@@ -2,11 +2,11 @@
 CREATE DATABASE wordpress;
 
 -- create a user and grant him all the privileges on the database 'wordpress'
-CREATE USER 'houda'@'%' IDENTIFIED BY 'thisisatest';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'houda'@'%';
+CREATE USER 'houda'@'localhost' IDENTIFIED BY 'thisisatest';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'houda'@'localhost';
 FLUSH PRIVILEGES;
 
 -- create a user and grant him the ability to select (READ) from all table withing the 'wordpress' database
-CREATE USER 'hamid'@'%' IDENTIFIED BY 'hamidou3elal';
-GRANT SELECT ON wordpress.* TO 'hamid'@'%';
+CREATE USER 'hamid'@'localhost' IDENTIFIED BY 'hamidou3elal';
+GRANT SELECT ON wordpress.* TO 'hamid'@'localhost';
 FLUSH PRIVILEGES;
