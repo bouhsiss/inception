4a003507af72af035ff935326b191b247e3ddc2e
Houda Bouhsiss <hbouhsis@e2r1p4.1337.ma>
2022-12-26 20:56:14 +0100

    done

--- srcs/requirements/mariadb/create_wp_db.sql
+++ srcs/requirements/mariadb/create_wp_db.sql
@@ -2,11 +2,14 @@
 CREATE DATABASE wordpress;
 
 -- create a user and grant him all the privileges on the database 'wordpress'
-CREATE USER 'houda'@'localhost' IDENTIFIED BY 'thisisatest';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'houda'@'localhost';
+CREATE USER 'houda'@'%' IDENTIFIED BY 'thisisatest';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'houda'@'%';
 FLUSH PRIVILEGES;
 
 -- create a user and grant him the ability to select (READ) from all table withing the 'wordpress' database
-CREATE USER 'hamid'@'localhost' IDENTIFIED BY 'hamidou3elal';
-GRANT SELECT ON wordpress.* TO 'hamid'@'localhost';
+CREATE USER 'hamid'@'%' IDENTIFIED BY 'hamidou3elal';
+GRANT SELECT ON wordpress.* TO 'hamid'@'%';
+FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'MyN3wP4ssw0rd';
 FLUSH PRIVILEGES;
--- srcs/requirements/wordpress/wp-config-sample.php
+++ srcs/requirements/wordpress/wp-config-sample.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'houda' );
 define( 'DB_PASSWORD', 'thisisatest' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
