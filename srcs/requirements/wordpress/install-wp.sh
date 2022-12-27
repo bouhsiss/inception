c4999ff2a80b3795f3638f396d89b40e733038d3
Houda Bouhsiss <hbouhsis@e2r1p4.1337.ma>
2022-12-27 18:23:25 +0100

    i guess final

--- srcs/requirements/wordpress/install-wp.sh
+++ srcs/requirements/wordpress/install-wp.sh
@@ -4,7 +4,4 @@ cd /var/www/html/wordpress
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x ./wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-sudo wp  core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} \
- --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
-sudo wp  config create --allow-root --dbname=${MYSQL_DATABASE} \
- --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST}
+sudo wp  core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
